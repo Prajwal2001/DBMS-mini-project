7ea4972f9fe921490e4124a690eed0c344f641a8
Prajwal Kulkarni <parashuramk237@gmail.com>
2021-12-08 17:26:36 +0530

    Many changes

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from json import dumps
 app = Flask(__name__)
 db = Database()
 isAuthorized = False
+user: int
 
 
 @app.route('/')
@@ -14,36 +15,47 @@ def redirect_pg():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    global isAuthorized
+    global isAuthorized, user
     if request.method == 'POST':
         if db.does_user_exist([request.form.get('username'), request.form.get('password')]):
             isAuthorized = True
+            user = db.get_user_id_by_user_name(request.form.get('username'))
             return redirect('/home', code=302)
         else:
             isAuthorized = False
             return render_template("index.html", status=False)
     else:
+        isAuthorized = False
         return render_template("index.html", status=True)
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    global isAuthorized, user
     if request.method == 'POST':
-        response = db.add_user([request.form.get('username'), request.form.get(
-            'email'), request.form.get('password')])
+        response = db.add_user([request.form.get('username'),
+                                request.form.get('email'),
+                                request.form.get('password')])
+        user = db.get_user_id_by_user_name(request.form.get('username'))
         return redirect('/home', code=302) if response else render_template("signup.html", status=response)
     else:
+        isAuthorized = False
         return render_template("signup.html", status=True)
 
 
 @app.route("/home")
 def home():
-    global isAuthorized
+    global isAuthorized, user
     if isAuthorized:
-        isAuthorized = False
-        return render_template("home.html")
+        return render_template("home.html", user=user)
     else:
         return redirect('/login', code=302)
+
+
+@app.route("/home/<int:user_id>/viewtickets")
+def viewtickets(user_id):
+    global user
+    return render_template("tickets.html", tickets=db.get_ticket(user_id), user=user)
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 import hashlib
 import mysql.connector
 import yaml
+from datetime import timedelta, date
 
 
 class Database:
@@ -17,10 +18,10 @@ class Database:
 
         self.__cursor = self.__mydb.cursor()
 
-    def add_user(self, user: list):
-        """Adds a user to the database, by accepting a list consisting of username, password and email.
+    def add_user(self, user_id: list):
+        """Adds a user_id to the database, by accepting a list consisting of username, password and email.
         And returns the updated table values in a list of key value pairs"""
-        u_name, email, password = user
+        u_name, email, password = user_id
 
         try:
             self.__cursor.execute(
@@ -37,7 +38,79 @@ class Database:
             "password": item[3]
         } for item in self.__cursor.fetchall()]
 
-    def does_user_exist(self, user: list):
+    def does_user_exist(self, user_id: list):
+        """Checks if the user_id exists in the database"""
         self.__cursor.execute(
-            f"select * from users where u_name = '{user[0]}' and password = '{hashlib.md5(user[1].encode()).hexdigest()}'")
+            f"""select * 
+            from users 
+            where u_name = '{user_id[0]}' and 
+            password = '{hashlib.md5(user_id[1].encode()).hexdigest()}'""")
         return bool(self.__cursor.fetchall())
+
+    def get_user_id_by_user_name(self, user_name):
+        """Returns user_id for given user_name"""
+        self.__cursor.execute(
+            f"select * from users where u_name = '{user_name}'")
+        return self.__cursor.fetchall()[0][0]
+
+    def get_ticket(self, user_id: int):
+        """Returns tickets for given user_id"""
+        def convert_date_format(date):
+            date = str(date).split('-')
+            return f"{date[-1]}-{date[-2]}-{date[-3]}"
+
+        def calculate_reaching_date(days, travelling_date):
+            temp = travelling_date.split('-')
+            travelling_date = date(int(temp[2]), int(temp[1]), int(temp[0]))
+            travelling_date = travelling_date + timedelta(int(days))
+            return convert_date_format(travelling_date)
+
+        self.__cursor.execute(
+            f"select pnr from tickets where user_id = {user_id}")
+        tickets_list = []
+        for pnr in self.__cursor.fetchall():
+
+            self.__cursor.execute(
+                f"""select T.pnr, travelling_date, T.train_no, TR.train_name, A.stat_loc, B.stat_loc, booking_date 
+                from tickets T join users U on T.user_id = U.user_id join trains TR on T.train_no = TR.train_no, stations A, stations B 
+                where T.pnr = { pnr[0] } and 
+                A.stat_id in (select from_station from tickets where pnr = { pnr[0] }) and 
+                B.stat_id in (select to_station from tickets where pnr = { pnr[0] });"""
+            )
+            ticket = self.__cursor.fetchall()
+
+            self.__cursor.execute(
+                f"""select T.pnr, p_name, p_age, seat_no 
+                from tickets T join users U on T.user_id = U.user_id join trains TR on T.train_no = TR.train_no join passengers P on P.pnr = T.pnr , stations A, stations B 
+                where T.pnr = { pnr[0] } and 
+                A.stat_id in (select from_station from tickets where pnr = { pnr[0] }) and 
+                B.stat_id in (select to_station from tickets where pnr = { pnr[0] });"""
+            )
+            passengers = self.__cursor.fetchall()
+
+            self.__cursor.execute(
+                f"select arrival_time, depart_time from covers where t_no = {ticket[0][2]} and s_id = (select stat_id from stations where stat_loc = '{ticket[0][4]}')")
+            source_timing = self.__cursor.fetchall()
+
+            self.__cursor.execute(
+                f"select arrival_time, days from covers where t_no = {ticket[0][2]} and s_id = (select stat_id from stations where stat_loc = '{ticket[0][5]}')")
+            destination_timing = self.__cursor.fetchall()
+
+            tickets_list.append(
+                {
+                    "pnr": ticket[0][0],
+                    "travelling_date": convert_date_format(ticket[0][1]),
+                    "train_no": ticket[0][2],
+                    "train_name": ticket[0][3],
+                    "from_station": ticket[0][4],
+                    "to_station": ticket[0][5],
+                    "passengers": [[passenger[1], passenger[2], passenger[3]] for passenger in passengers if passenger[0] == ticket[0][0]],
+                    "booking_date": convert_date_format(ticket[0][6]),
+                    "arrival_time": source_timing[0][0],
+                    "depart_time": source_timing[0][1],
+                    "reaching_time": destination_timing[0][0],
+                    "reaching_date": calculate_reaching_date(destination_timing[0][1], convert_date_format(ticket[0][1]))
+                }
+            )
+
+        return tickets_list
