28f256f235c23b6c7a02885890850ed51099ed08
Prajwal Kulkarni <parashuramk237@gmail.com>
2022-01-03 08:49:16 +0530

    Added stored procedure

--- database.py
+++ database.py
@@ -47,18 +47,19 @@ class Database:
     def does_user_exist(self, userData: list):
         """Checks if the user_id exists in the database"""
         self.__cursor.execute(
-            f"""SELECT * 
-            FROM users 
-            WHERE user_name = '{userData[0]}' AND 
-            password = '{hashlib.md5(userData[1].encode()).hexdigest()}'""")
-        res = self.__cursor.fetchall()
-        return res
+            f"""SELECT *
+                FROM users
+                WHERE user_name = '{userData[0]}' AND
+                password = '{hashlib.md5(userData[1].encode()).hexdigest()}'"""
+        )
+        result = self.__cursor.fetchall()
+        return result
 
     def get_user_id_by_user_name(self, userName):
         """Returns user_id for given user_name"""
         self.__cursor.execute(f"""
-            SELECT * 
-            FROM users 
+            SELECT *
+            FROM users
             WHERE user_name = '{userName}';
             """)
         res = self.__cursor.fetchall()
@@ -79,54 +80,41 @@ class Database:
 
         self.__cursor.execute(
             f"""
-            SELECT pnr 
-            FROM tickets 
+            SELECT pnr
+            FROM tickets
             WHERE user_id = {userId}
             """)
-        tickets_list = []
+        ticketsList = []
         for pnr in self.__cursor.fetchall():
+            self.__cursor.callproc("get_tickets", [pnr[0]])
+            for res in self.__cursor.stored_results():
+                ticket = res.fetchall()
 
-            self.__cursor.execute(
-                f"""SELECT T.pnr, travel_date, T.train_no, TR.train_name, A.stat_loc, B.stat_loc, booking_date, T.price 
-                FROM tickets T JOIN users U on T.user_id = U.user_id JOIN trains TR on T.train_no = TR.train_no, stations A, stations B 
-                WHERE T.pnr = { pnr[0] } AND 
-                    A.stat_id IN (SELECT from_station FROM tickets WHERE pnr = { pnr[0] }) AND 
-                    B.stat_id IN (SELECT to_station FROM tickets WHERE pnr = { pnr[0] })
-                ORDER BY travel_date;"""
-            )
-            ticket = self.__cursor.fetchall()
-
-            self.__cursor.execute(
-                f"""SELECT T.pnr, p_name, p_age, seat_no 
-                FROM tickets T JOIN users U on T.user_id = U.user_id JOIN trains TR on T.train_no = TR.train_no join passengers P on P.pnr = T.pnr , stations A, stations B 
-                WHERE T.pnr = { pnr[0] } AND 
-                    A.stat_id IN (SELECT from_station FROM tickets WHERE pnr = { pnr[0] }) AND 
-                    B.stat_id IN (SELECT to_station FROM tickets WHERE pnr = { pnr[0] })
-                ORDER BY seat_no;"""
-            )
-            passengers = self.__cursor.fetchall()
+            self.__cursor.callproc("get_passengers", [pnr[0]])
+            for res in self.__cursor.stored_results():
+                passengers = res.fetchall()
 
             self.__cursor.execute(
                 f"""
-                SELECT arrival_time, depart_time 
-                FROM covers 
-                WHERE train_no = {ticket[0][2]} AND 
-                    stat_id = (SELECT stat_id 
+                SELECT arrival_time, depart_time
+                FROM covers
+                WHERE train_no = {ticket[0][2]} AND
+                    stat_id = (SELECT stat_id
                                 FROM stations
                                 WHERE stat_loc = '{ticket[0][4]}')""")
-            source_timing = self.__cursor.fetchall()
+            sourceTiming = self.__cursor.fetchall()
 
             self.__cursor.execute(
                 f"""
-                SELECT arrival_time, days 
-                FROM covers 
+                SELECT arrival_time, days
+                FROM covers
                 WHERE train_no = {ticket[0][2]} AND
-                    stat_id = (SELECT stat_id 
-                                FROM stations 
+                    stat_id = (SELECT stat_id
+                                FROM stations
                                 WHERE stat_loc = '{ticket[0][5]}')""")
             destinationTiming = self.__cursor.fetchall()
 
-            tickets_list.append(
+            ticketsList.append(
                 {
                     "pnr": ticket[0][0],
                     "travelling_date": self.convert_date_format(ticket[0][1]),
@@ -136,15 +124,15 @@ class Database:
                     "to_station": ticket[0][5],
                     "passengers": [[passenger[1], passenger[2], passenger[3]] for passenger in passengers if passenger[0] == ticket[0][0]],
                     "booking_date": self.convert_date_format(ticket[0][6]),
-                    "arrival_time": source_timing[0][0],
-                    "depart_time": source_timing[0][1],
+                    "arrival_time": sourceTiming[0][0],
+                    "depart_time": sourceTiming[0][1],
                     "reaching_time": destinationTiming[0][0],
                     "reaching_date": self.calculate_reaching_date(destinationTiming[0][1], self.convert_date_format(ticket[0][1])),
                     "price": ticket[0][7]
                 }
             )
 
-        return tickets_list
+        return ticketsList
 
     def get_stations(self):
         self.__cursor.execute("SELECT stat_name FROM stations")
@@ -154,17 +142,17 @@ class Database:
         trains_list = []
         self.__cursor.execute(
             f"""
-            SELECT A.stat_id, B.stat_id 
-            FROM stations A, stations B 
-            WHERE A.stat_name = '{sourceName}' AND 
+            SELECT A.stat_id, B.stat_id
+            FROM stations A, stations B
+            WHERE A.stat_name = '{sourceName}' AND
                 B.stat_name = '{destinationName}';""")
         source, destination = self.__cursor.fetchall()[0]
         self.__cursor.execute('SELECT train_no FROM trains')
         train_nos = self.__cursor.fetchall()
         for trainNo in train_nos:
             self.__cursor.execute(
-                f"""SELECT seq_no, arrival_time 
-                FROM covers 
+                f"""SELECT seq_no, arrival_time
+                FROM covers
                 WHERE train_no = {trainNo[0]} AND stat_id = {destination};""")
             destData = self.__cursor.fetchall()
             if destData:
@@ -172,8 +160,8 @@ class Database:
             else:
                 destSeqno = None
             self.__cursor.execute(
-                f"""SELECT seq_no, arrival_time, depart_time 
-                FROM covers 
+                f"""SELECT seq_no, arrival_time, depart_time
+                FROM covers
                 WHERE train_no = {trainNo[0]} AND stat_id = {source};""")
             sourceData = self.__cursor.fetchall()
             if sourceData:
@@ -247,12 +235,9 @@ class Database:
         return price
 
     def get_train_details(self):
-        self.__cursor.execute("""
-        SELECT train_no, train_name, stat_name, arrival_time, depart_time 
-        FROM all_train_info 
-        ORDER BY train_no, seq_no;
-        """)
-        trainDetails = self.__cursor.fetchall()
+        self.__cursor.callproc("trains_info")
+        for result in self.__cursor.stored_results():
+            trainDetails = result.fetchall()
         trainNos = []
         for tr in [train[0] for train in trainDetails]:
             if tr not in trainNos:
@@ -261,7 +246,8 @@ class Database:
         for tr in [train[1] for train in trainDetails]:
             if tr not in trainNames:
                 trainNames.append(tr)
-        trainsList = [{
+
+        return [{
             "train_no": trainNos[i],
             "train_name": trainNames[i],
             "stations_list": [tr[2] for tr in trainDetails if trainNos[i] == tr[0]],
@@ -270,6 +256,5 @@ class Database:
             "no_of_stations": len([tr[2] for tr in trainDetails if trainNos[i] == tr[0]])
         } for i in range(len(trainNos))]
 
-        return trainsList
 
 # DATE_SUB(CURDATE(), INTERVAL 1 DAY)
